3fa1a2e0c7140e2c697a835c4e2c19fe815c4637
a.nejadipour <nejadipour@gmail.com>
2024-03-15 17:10:24 +0330

    Update package.json and readme

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from "./app/index.jsx";
+import Datepicker from "./components/Datepicker/index.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-)
+export default Datepicker;
